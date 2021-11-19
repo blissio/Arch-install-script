0dc861019ae2ad297642d5ff645f0eca08f905bc
RealBlissIO <94686050+RealBlissIO@users.noreply.github.com>
2021-11-19 14:44:47 +0100

    Update arch-install-script.sh

--- arch-install-script.sh
+++ arch-install-script.sh
@@ -27,7 +27,7 @@ fi
 mount $partition /mnt 
 pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-sed '1,/^#part2$/d' arch_install.sh > /mnt/arch_install2.sh
+sed '1,/^#part2$/d' arch_install-script.sh > /mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
 exit 
